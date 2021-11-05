a44c077667e9e2a964ae6cb0237eee19a78d1873
Ruben <Pinto>
2021-11-05 19:58:23 +0000

    Add new features and pass parameter.

--- gacusto3.m
+++ gacusto3.m
@@ -1,6 +1,8 @@
 %Programa de optimizacao
 %X = GA(FITNESSFCN,NVARS,A,b,Aeq,beq,lb,ub,NONLCON,options)
+function [x,fval] = gacusto3(a, b)
 options = gaoptimset('PlotFcns',...
  {@gaplotbestf,@gaplotbestindiv,@gaplotexpectation,@gaplotstopping},...
- 'PopInitRange',[0;0.9]);
-[x fval] = ga(@custo,2,[1 0],0.5,[1 1],0.9,0,0.9,[],options)
+ 'PopInitRange',[a;b]);
+[x, fval] = ga(@custo,2,[1 0],0.5,[1 1],0.9,0,0.9,[],options);
+
