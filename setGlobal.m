9efd33efed49cd3e1b6f41a0318278495af49674
Ruben <Pinto>
2021-11-26 18:36:45 +0000

    Add new features

--- setGlobal.m
+++
@@ -1,7 +0,0 @@
-function setGlobal(valp1, valp2, valp3)
-global p1
-global p2
-global p3
-p1 = valp1;
-p2 = valp2;
-p3 = valp3;
---
+++ setGlobal2.m
@@ -0,0 +1,11 @@
+function setGlobal2(valp1, valp2, valp3, valp4, valp5)
+global pg1
+global pg1_2
+global pg2
+global pg2_2
+global p3
+pg1 = valp1;
+pg1_2 = valp2;
+pg2 = valp3;
+pg2_2 = valp4;
+p3 = valp5;
