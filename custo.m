e0f5ffc2e7b573c01999d786f201c96f06f66890
themrxpro <fpi.informatica@gmai.com>
2021-12-14 00:23:45 +0000

    Added conditions to GA

--- custo.m
+++ custo.m
@@ -5,14 +5,14 @@ function z=custo(x)
     global x12 x23 x13;
     global c11 c12 c21 c22;
     
-    %if ex2 ou 3
-    pg1 = x(1)
-    pg2 = x(2)
-    %if ex4 ou mais
-
-    pg1 = x(1)*(1/x12 + 1/x23) - x(2)/x23;
-    pg2 = x(2)*(1/x13 + 1/x23) - x(1)/x23;
-    
+    if ~NODAL
+        pg1 = x(1)
+        pg2 = x(2)
+    else
+        pg1 = x(1)*(1/x12 + 1/x23) - x(2)/x23;
+        pg2 = x(2)*(1/x13 + 1/x23) - x(1)/x23;
+    end
+        
     c1 = c11 * pg1 + c12 * pg1^2;
     c2 = c21 * pg2 + c22 * pg2^2;
     z=c1+c2;
--- gacusto.m
+++ gacusto.m
@@ -6,70 +6,90 @@ function [x,fval] = gacusto()
 
     %loading variables
     
-    plots={@gaplotbestf,@gaplotbestindiv,@gaplotexpectation,@gaplotstopping};
+    %plots={@gaplotbestf,@gaplotbestindiv,@gaplotexpectation,@gaplotstopping};
     
-    %global pg1 pg1_2 pg2 pg2_2; 
-    global x12 x23 x13;
+
+    global x12 x23 x13 NODAL;
     global c11 c12 c21 c22;
     
     load cachefile;
-    % x's e c's
-    % carregar e substituir se ex*2,3 ou 4
-    %falta PopSize InitRange [a,b] plots
-    
-    [x12, x23, x13] = deal( df );
-    x12=0.3;
-    x23=0.1;
-    x13=0.1;
-    c11=0.3;
-    c12=0.01;
-    c21=0.2;
-    c22=0.3;
-    
+
     %Setting options
-    options = optimoptions('ga',...     % genetic algorithm
-        'PopulationSize',   PopSize,...   % numero da população
+    options = optimoptions('ga',...     
+        'PopulationSize',   PopSize,...   
         'PopInitRange',     InitRange,...
-        'MaxGenerations',   MaxGen,...    % 
-        'MaxTime',          MaxTime,...         %
+        'MaxGenerations',   MaxGen,...    
+        'MaxTime',          MaxTime,...         
         'InitialPenalty',   InitPen,...
         'PenaltyFactor',    PenFactor,...
         'PlotFcn',          plots,...
-        'SelectionFcn',     Selection,...   %
+        'SelectionFcn',     Selection,...   
         'EliteCount',       EliteCount,...
         'CrossoverFraction',CrossFract,...
-        'MutationFcn',      Mutation,...     %
-        'CrossoverFcn',     Crossover);     %
+        'MutationFcn',      Mutation,...     
+        'CrossoverFcn',     Crossover);     
+    % ex2
+    if      Exercise == "Ex2"
+        [x, fval] = ga(@custo, 2,    [],   [],  [1 1],  0.9,  0,  1,  [], options);
         
-    %Nao Limites
-    A=[];
-    b=[];
-    %Limites <= pg1
-    A=[1 0];
-    b=[0.5];%limite
-    %Limites <= pg2
-    A=[0 1];
-    b=[0.5];%limite
-    
-    %if tensoes nodais
-    
-    
-    %Function call 
-    [x, fval] = ga(@custo,...
-        2       ,...<Number of design variables>
-        A       ,...<A matrix for inequality constraints>   %x1 ou x2
-        b       ,...<b vector for inequality constraints>   %<= ao valor
-        [1 1]   ,...<Aeq matrix for equality constraints>   %x1 +x2 =
-        0.9     ,...<beq vector for equality constraints>   %0.9 Potencia de saida
-        0       ,...<Lower bound on X> limite inferior
-        2*pi    ,...<Upper bound on X> limite superior
-        []      ,...NONLCON
-        options);
-    
+    elseif  Exercise == "Ex3"
+        [x, fval] = ga(@custo, 2, [1 0], [0.5], [1 1],  0.9,  0,  1,  [], options);
+        
+    elseif  Exercise == "Ex4"
+        [x, fval] = ga(@custo, 2,    [], [], [10 -20],  0.9, [], [],  [], options);
+        
+    else
+        %EX Customizado
+        if pg1_max==true || pg2_max==true % Redundant but does not work otherwise
+            %Output restriction
+            if ~NODAL % NORMAL
+                if pg1_max==true % Pg1
+                    A=[1 0];    
+                    b=[pg1_max];
+                else % Pg2
+                    A=[0 1];    
+                    b=[pg2_max];
+                end
+            else % NODAL
+                if pg1_max==true % Pg1
+                    A=[(1/x12 + 1/x23) -1/x23];    
+                    b=[pg1_max-p_max];
+                else % Pg2
+                    A=[0 1];    
+                    b=[pg2_max];
+                end
+            end
+        else
+            %No Output Restriction
+            A=[];   
+            b=[];
+        end
+        [x, fval] = ga(@custo,...
+            2       ,...<Number of design variables>
+            A       ,...<A matrix for inequality constraints>   %x1 ou x2
+            b       ,...<b vector for inequality constraints>   %<= ao valor
+            [1 1]   ,...<Aeq matrix for equality constraints>   %x1 +x2 =
+            P1+P2   ,...<beq vector for equality constraints>   %0.9 Potencia de saida
+            0       ,...<Lower bound on X> limite inferior
+            P1+P2   ,...<Upper bound on X> limite superior
+            []      ,...NONLCON
+            options);
+    end
+
     %saving variables
-    x1 = num2str(x(1),3);
-    x2 = num2str(x(2),3);
-    x3 = num2str(fval, 3);
-    x4 = num2str(x(1) + x(2), 3);
-    save('cachefile.mat', 'x1','x2','x3','x4','-append');
+    if (Exercise == "Ex2" || Exercise == "Ex3") 
+        p1 = num2str(x(1),3);
+        p2 = num2str(x(2),3);
+        t2 = '';
+        t3 = '';
+    else
+        p1 = pg1;
+        p2 = pg2;
+        t2 = num2str(x(1),3);
+        t3 = num2str(x(2),3);
+    end
+    pt = num2str(p1+p2, 3); 
+    fit = num2str(fval, 3);
+    
+    save('cachefile.mat','p1','p2','pt','t2','t3','fit','-append');
 
